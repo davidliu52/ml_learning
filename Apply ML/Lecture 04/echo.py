d98f6d913c0126b3d15a7b10b6053b140d6621a3
lia68085 <lian.liu@ipk.fraunhofer.de>
2023-11-14 15:36:45 +0100

    aaaaa

---
+++ Apply ML/Lecture 04/echo.py
@@ -0,0 +1,21 @@
+
+"""
+Explain the __init__ main lines
+
+from: https://realpython.com/if-name-main-python/
+"""
+
+def echo(text: str, repetitions: int = 5) -> str:
+    """Imitate a real-world echo."""
+    echoed_text = ""
+    for i in range(repetitions, 0, -1):
+        echoed_text += f"{text[-i:]}\n"
+    return f"{echoed_text.lower()}."
+
+
+if __name__ == "__main__":
+    text = 'hello'
+    print(echo(text))
+
+# text = 'hello'
+# print(echo(text))
